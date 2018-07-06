34e33fc592dce6f05fafa6c12cca5685c954dac7
Иван Питенин <ivan.pitenin@gmail.com>
2018-07-06 19:05:04 +0300

    fix dropdown panel

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,7 +1,12 @@
 var kursy = document.querySelector('.kursy');
 kursy.addEventListener('click', function (e) {
-  var kursItem = e.target.parentNode.parentNode.parentNode;
-  if (e.target.classList.contains('pop-arrow')) {
+  var kursItem = e.target.parentNode.parentNode;
+  if (e.target.parentNode.classList.contains('kursy__lvl-title') || e.target.parentNode.classList.contains('kursy__lvl-top')) {
+    if (kursItem.classList.contains('kursy__lvl-top')) {
+      kursItem = e.target.parentNode.parentNode.parentNode;
+    } else {
+      kursItem = e.target.parentNode.parentNode;
+    }
     if (kursItem.classList.contains('kursy__lvl--closed')) {
       kursItem.classList.remove('kursy__lvl--closed');
       kursItem.classList.add('kursy__lvl--opened');
