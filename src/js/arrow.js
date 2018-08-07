335d4ad2ef9d7afce9960c07930ff3c185ee1c9f
Иван Питенин <ivan.pitenin@gmail.com>
2018-08-07 15:55:15 +0300

    many fix + search

--- src/js/arrow.js
+++ src/js/arrow.js
@@ -2,12 +2,13 @@
 var dropdownList = document.querySelector('.prime__descr--dropdown');
 var dropdownArrow = document.querySelector('.dropdown-arrow');
 dropdownArrow.addEventListener('click', function () {
-  if (dropdownList.style.height == '100%') {
+  var dropdownListHeight = getComputedStyle(dropdownList.querySelector('.prime__descr-content')).height;
+  if (dropdownList.style.height == dropdownListHeight) {
     dropdownList.style.height = '240px';
     dropdownArrow.classList.remove('dropdown-arrow--open');
 
   } else {
-    dropdownList.style.height = '100%';
+    dropdownList.style.height = dropdownListHeight;
     dropdownArrow.classList.add('dropdown-arrow--open');
   }
 });
